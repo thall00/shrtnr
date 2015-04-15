05bcef12d27897838693c83a153aba67a485590b
Ben Woodall <mail@benwoodall.com>
2015-04-15 15:21:59 -0700

    add multipart example and added attachment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   default from: "mail@benwoodall.com"
 
   def welcome_email(user)
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/public/assets/logo.png")
     @user = user
     mail(to: user.email, subject: "Welcome to Shortener!")
   end
