3af105bcad87c5e0ba63bfee1d19c84baf9259f8
Tim Hall <timhall3000@gmail.com>
2015-06-29 22:37:18 -0700

    Added integration test for creating a User

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -33,5 +33,20 @@ describe "creating a user" do
     it "tells the user they made a mistake" do
       expect(page).to have_content "Password confirmation doesn't match Password"
     end
+
+    before do
+      test_user = User.create name:"test", email:"test@test.com", password_digest:"test"
+      visit "/users/new"
+      fill_in "Email", with: "test@test.com"
+      fill_in "Password", with: "Password"
+      fill_in "Password confirmation", with: "NOPE"
+      click_button "Submit"
+    end
+
+    it "tells the user they made a mistake" do
+      expect(page).to have_content "Email has already been taken"
+    end
+    #test_user.destroy
+
   end
 end
