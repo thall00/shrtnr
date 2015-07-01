ce23d68fa9c2ac3d1509bec57bb9fb2e36ff83c6
Tim Hall <timhall3000@gmail.com>
2015-06-30 19:18:35 -0700

    Added itegration test for updating settings when signed in.

--- spec/features/update_settings_spec.rb
+++ spec/features/update_settings_spec.rb
@@ -1,23 +1,26 @@
 require 'spec_helper'
 
 describe "updating settings" do
-  context "with valid params" do
+  context "when signed in" do
+    let(:user) { create(:user) }
 
     before do
+      login_as user
       visit "/settings"
       puts page.body
       fill_in "Name", with: "Test"
       fill_in "Email", with: "test@test.com"
-      click_button "Submit"
+      click_button "Update"
     end
 
     it "tells the user they have updated their settings" do
       #expect(page).to have_content "Successfully updated settings"
     end
-
     it "sends them to the settings page" do
       #expect(current_path).to eq "/settings"
     end
+  end
+  context "with valid params" do
 
   end
 end
