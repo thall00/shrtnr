06785336d02c649bcb58e64b78cac8ad3826bb26
Tim Hall <tim.hall@beatsbydre.com>
2015-07-22 17:25:38 -0700

    Added SettingsMailer with update_email, and triggered it in SettingsController

--- app/mailers/settings_mailer.rb
+++ app/mailers/settings_mailer.rb
@@ -1,2 +1,10 @@
 class SettingsMailer < ApplicationMailer
+  default from: "mail@benwoodall.com"
+  
+  def update_email(user)
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/public/assets/logo.png")
+    @user = user
+    mail(to: user.email, subject: "You have updated your settings!")
+  end
+
 end
